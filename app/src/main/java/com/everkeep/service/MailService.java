68846426ea269a3e875205cdef0d7149d6a36035
shveykinvlad <shveykinvlad@gmail.com>
2022-07-15 00:04:36 +0200

    Add email message codes constants

--- app/src/main/java/com/everkeep/service/MailService.java
+++ app/src/main/java/com/everkeep/service/MailService.java
@@ -15,21 +15,26 @@ import com.everkeep.config.properties.MailProperties;
 @RequiredArgsConstructor
 public class MailService {
 
+    private static final String CONFIRMATION_SUBJECT_CODE = "user.confirm.email.subject";
+    private static final String CONFIRMATION_MESSAGE_CODE = "user.confirm.email.message";
+    private static final String RESET_PASSWORD_SUBJECT_CODE = "user.password.reset.email.subject";
+    private static final String RESET_PASSWORD_MESSAGE_CODE = "user.password.reset.email.message";
+
     private final JavaMailSender javaMailSender;
     private final MailProperties mailProperties;
     private final IntegrationProperties integrationProperties;
     private final MessageSource messageSource;
 
     public void sendUserConfirmationMail(String to, String tokenValue) {
-        var subject = getDefaultMessage("user.confirm.email.subject");
-        var text = getDefaultMessage("user.confirm.email.message", integrationProperties.getUiUrl(), tokenValue);
+        var subject = getDefaultMessage(CONFIRMATION_SUBJECT_CODE);
+        var text = getDefaultMessage(CONFIRMATION_MESSAGE_CODE, integrationProperties.getUiUrl(), tokenValue);
 
         send(to, subject, text);
     }
 
     public void sendResetPasswordMail(String to, String tokenValue) {
-        var subject = getDefaultMessage("user.password.reset.email.subject");
-        var text = getDefaultMessage("user.password.reset.email.message", integrationProperties.getUiUrl(), to, tokenValue);
+        var subject = getDefaultMessage(RESET_PASSWORD_SUBJECT_CODE);
+        var text = getDefaultMessage(RESET_PASSWORD_MESSAGE_CODE, integrationProperties.getUiUrl(), to, tokenValue);
 
         send(to, subject, text);
     }
