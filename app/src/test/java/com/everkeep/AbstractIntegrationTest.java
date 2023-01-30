4f90625de3cc8835eacd78cdd8c69314930f4306
shveykinvlad <shveykinvlad@gmail.com>
2023-01-30 14:05:08 +0100

    Fix TestContainers configuration

--- app/src/test/java/com/everkeep/AbstractIntegrationTest.java
+++ app/src/test/java/com/everkeep/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.everkeep;
 
-import com.everkeep.config.DatabaseConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
@@ -10,15 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DatabaseConfig.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class AbstractIntegrationTest extends AbstractTest {
 
     private static final String EMAIL_USER = "login";
     private static final String EMAIL_PASSWORD = "password";
+
+    private static final JdbcDatabaseContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:14.4");
 
     @RegisterExtension
     protected static final GreenMailExtension GREEN_MAIL = new GreenMailExtension(ServerSetupTest.SMTP)
@@ -33,4 +38,12 @@ public abstract class AbstractIntegrationTest extends AbstractTest {
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper mapper;
+
+    @DynamicPropertySource
+    private static void databaseProperties(DynamicPropertyRegistry registry) {
+        POSTGRES_CONTAINER.start();
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+    }
 }
